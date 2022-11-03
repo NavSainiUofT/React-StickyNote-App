3b2bc9b9936111ff74e71781d708b3bec77568ce
NavSainiUofT <thenavsta@gmail.com>
2022-11-03 13:42:51 -0400

    adding custom note creation and delete all

--- src/components/form.js
+++ src/components/form.js
@@ -5,19 +5,29 @@ import { Sticky } from "./stickynote";
 
 export function Form() {
   const [stickyList, setStickyList] = useState([]);
+  const [inputState, setInputState] = useState({value: ""});
 
   const onAddBtnClick = event => {
-    setStickyList(stickyList.concat(<Sticky props = {{text:"new sticky note text"}} key={stickyList.length}/>));
+    setStickyList(stickyList.concat(<Sticky props = {{text:inputState.value}} key={stickyList.length}/>));
     console.log(stickyList);
   };
-  const onDeleteAllBtnClick = even => {
+  const onDeleteAllBtnClick = event => {
     setStickyList([])
   };
+  const handleChange = (e) => {
+    setInputState({value: e.target.value});
+  };
+  
 
   return (
     <div>
       <button onClick={onAddBtnClick}>Add StickyNote</button>
       <button onClick={onDeleteAllBtnClick}>Delete ALL Sticky Notes</button>
+        <input
+          type="text"
+          value={inputState.value}
+          onChange={handleChange}
+        />
       {stickyList}
     </div>
   );
