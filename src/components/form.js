4eed982ff9e9d05cb1aec620278b75ddea9928bb
NavSainiUofT <thenavsta@gmail.com>
2022-11-03 11:53:32 -0400

    delete all button

--- src/components/form.js
+++ src/components/form.js
@@ -8,11 +8,16 @@ export function Form() {
 
   const onAddBtnClick = event => {
     setStickyList(stickyList.concat(<Sticky props = {{text:"new sticky note text"}} key={stickyList.length}/>));
+    console.log(stickyList);
+  };
+  const onDeleteAllBtnClick = even => {
+    setStickyList([])
   };
 
   return (
     <div>
       <button onClick={onAddBtnClick}>Add StickyNote</button>
+      <button onClick={onDeleteAllBtnClick}>Delete ALL Sticky Notes</button>
       {stickyList}
     </div>
   );
